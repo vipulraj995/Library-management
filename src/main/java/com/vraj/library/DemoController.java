7cba77f3899d9b2f8475a64542f344438276d29e
vipul raj <vipul raj@Vipul-PC>
2020-08-21 02:04:35 +0530

    Initial commit 1.

---
+++ src/main/java/com/vraj/library/DemoController.java
@@ -0,0 +1,13 @@
+package com.vraj.library;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hi! Finally set up hogya..";
+	}
+}
